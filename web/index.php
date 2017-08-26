385fd13e7cb52e906fce9080142cd44e3dc741c1
CHY0919 <b9932028@gmail.com>
2017-08-26 15:52:41 +0800

    info

--- web/index.php
+++ web/index.php
@@ -26,41 +26,7 @@
 	// 	}
 	 
 	 phpinfo();
-	// 將收到的資料整理至變數
-	$receive = json_decode(file_get_contents("php://input"));
 	
-	// 讀取收到的訊息內容
-	$text = $receive->events[0]->message->text;
-	
-	// 讀取訊息來源的類型 	[user, group, room]
-	$type = $receive->events[0]->source->type;
-	
-	// 由於新版的Messaging Api可以讓Bot帳號加入多人聊天和群組當中
-	// 所以在這裡先判斷訊息的來源
-	if ($type == "room")
-	{
-		// 多人聊天 讀取房間id
-		$from = $receive->events[0]->source->roomId;
-	} 
-	else if ($type == "group")
-	{
-		// 群組 讀取群組id
-		$from = $receive->events[0]->source->groupId;
-	}
-	else
-	{
-		// 一對一聊天 讀取使用者id
-		$from = $receive->events[0]->source->userId;
-	}
-	
-	// 讀取訊息的型態 [Text, Image, Video, Audio, Location, Sticker]
-	$content_type = $receive->events[0]->message->type;
-	
-	// 準備Post回Line伺服器的資料 
-	$header = ["Content-Type: application/json", "Authorization: Bearer {" . $channel_access_token . "}"];
-	
-	// 回覆訊息
-	reply($content_type, $text);
 
 	
 	function reply($content_type, $message) {
