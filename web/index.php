caa223e187dc048e8bfade6d1e2b28f7339afa9e
arthur0881 <arthur0882@gmail.com>
2017-12-13 15:42:38 +0800

    time report

--- web/index.php
+++ web/index.php
@@ -22,7 +22,10 @@ require_once('./crawl_page.php');
 date_default_timezone_set("Asia/Taipei");
 
 echo date('Y-m-d H:i:s');
-echo get_day(date('d'));
+echo date('H');
+echo date('i');
+echo date('s');
+//echo get_day(date('d'));
 //echo get_r(4);
 //echo get_choice1("1 2 3 4 1個替代役");
 //echo get_choice2("1 2 3 4 5 6 7 2個替代役");
@@ -76,16 +79,12 @@ foreach ($client->parseEvents() as $event) {
                 		$m_message = "阿勇";
                 		
                 	}
-					else if(date('H') == "15" && date('i') == "31" && date('s') == "00")
+					else if((string)date('H') == "15" && (string)date('i') == "44" && (string)date('s') == "00")
                 	{
                 		$m_message = "你好";
                 		
                     }
-					else if($message['text']=="QAQ")
-                	{
-                		
-                		bot.push('test');
-                	}
+				
                 	else if(stristr($message['text'],"號班表"))
                 	{
                 		$m_day = preg_replace('/[^\d]/','',$message['text']);
